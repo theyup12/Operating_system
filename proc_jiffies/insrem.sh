7c43659c35d70916f7e2086b609f2a8ccff44382
theyup12 <46938075+theyup12@users.noreply.github.com>
2021-02-23 14:30:07 -0800

    Add files via upload

---
+++ proc_jiffies/insrem.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+​
+# clear the message queue, then
+# insert simple.ko into the kernel and remove it again, then
+# display the messages as a result of doing so
+​
+sudo -p "osc"
+sudo dmesg -c 
+clear 
+
+printf "beginning script...\n\n"
+sudo insmod jiffies.ko
+printf "cat /proc/jiffies: "
+cat /proc/jiffies
+sudo rmmod jiffies
+sudo dmesg 
+printf "\n...end of script\n\n"
